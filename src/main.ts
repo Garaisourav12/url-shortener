defa854792f24fc8ae787bae2ce65572f61e0f5e
Garaisourav12 <garaisourav12@gmail.com>
2025-07-29 20:55:30 +0530

    Api doc by swager

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('URL Shortener API')
+    .setDescription('API for shortening URLs')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   app.useGlobalFilters(new AllExceptionsFilter());
   app.use(cookieParser());
   console.log(`http://localhost:${process.env.PORT ?? 3000}`);
